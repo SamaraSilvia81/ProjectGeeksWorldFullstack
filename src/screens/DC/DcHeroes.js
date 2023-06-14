309b91e95a04004a0ef147356f499028ccd416bb
Eudes Pereira <99928327+eudescpereira@users.noreply.github.com>
2023-06-13 22:27:54 -0300

    Added pages Marvel and DC with logic of the characters filtered by enterprise

---
+++ src/screens/DC/DcHeroes.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import { ActivityIndicator, View, StatusBar, StyleSheet, FlatList } from "react-native";
+import { Text } from 'react-native-paper';
+
+import { useNavigation } from '@react-navigation/native';
+import { useQuery } from "@tanstack/react-query";
+
+import { CardCharacters } from "../../components/CardCharacters";
+import { getCharacter } from "../../backend/api";
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+function DCHeroes () {
+
+  const [characterType, setCharacterType] = useState("hero"); // Estado local para controlar o tipo de personagem (herói ou vilão)
+
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ["WorldsGeekBackend"],
+    queryFn: getCharacter,
+  });
+
+  const navigation = useNavigation();
+
+  const handleCardPress = (character) => {
+    navigation.navigate('DCHeroesChar', { characterId: character.id });
+  };
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Loading</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  // Filtra os personagens com base no tipo selecionado
+  const filteredData = data.filter((character) => {
+    if (!character.isMarvel && character.isHero && characterType === "hero") {
+      return true;
+    } else if (character.isMarvel && !character.isHero && characterType === "villain") {
+      return true;
+    }
+    return false;
+  });
+
+  return (
+    <View style={styles.container}>
+      {isFetching && <Text>IS FETCHING</Text>}
+
+      <StatusBar
+        barStyle="dark-content"
+        hidden={false}
+        backgroundColor="transparent"
+        translucent={false}
+        networkActivityIndicatorVisible={true}
+      />
+
+      <View style={styles.arrowIconContainer}>
+        <Icon
+          name="arrow-back"
+          size={25}
+          color="#FFFFFF"
+          onPress={handleGoBack}
+        />
+      </View>
+
+      <View style={{ flex: 1 }}>
+        <FlatList
+          style={{ flex: 1 }}
+          data={filteredData}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <CardCharacters character={item} onPress={handleCardPress} />
+          )}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#fcfcfc',
+  },
+  arrowIconContainer: {
+    position: 'absolute',
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 100,
+    padding: 5,
+    top: 20,
+    left: 25,
+    zIndex: 1,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "blue",
+  },
+});
+
+export default DCHeroes;
---
+++ src/screens/DC/DcVillChar.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+import { View, ActivityIndicator, StyleSheet } from "react-native";
+import { Text } from "react-native-paper";
+
+import { useQuery } from "@tanstack/react-query";
+
+import CharDetails from "../../components/CharDetails";
+import { getCharacter } from "../../backend/api";
+
+function DcVillChar({ route }) {
+  
+  const { characterId } = route.params;
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["WorldsGeekBackend"],
+    queryFn: getCharacter,
+  });
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.errorContainer}>
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  const selectedCharacter = data.find((character) => character.id === characterId);
+
+  if (!selectedCharacter) {
+    return (
+      <View style={styles.errorContainer}>
+        <Text>No hero found</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <CharDetails character={selectedCharacter} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fcfcfc",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default DcVillChar;
